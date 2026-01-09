ec375f338098c17cec8f6d8d937fd2e37da7e5e2
Felipe Maza <felipe@felipem.com>
2026-01-09 10:59:32 +0100

    Merge these implicitly concatenated strings; or did you forget a comma?

--- src/templatelib/tests/__init__.py
+++ src/templatelib/tests/__init__.py
@@ -8,4 +8,4 @@ def run_tests():
     """run all available tests"""
     tests_path = Path(__file__).parent
     print(f"Running tests from path: {tests_path}")
-    pytest.main(["-v", "-m", "not slow", "-p" "no:warnings", tests_path])
+    pytest.main(["-v", "-m", "not slow", "-p", "no:warnings", tests_path])
